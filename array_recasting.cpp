3c44f7576f470ef0acf3263d2bb7ede075b88c52
Joshua DM Hellier <joshua.hellier-2@manchester.ac.uk>
2024-03-18 18:47:37 +0000

    Make example setup for checking how array recasting works.

---
+++ array_recasting.cpp
@@ -0,0 +1,42 @@
+#include "array_recasting.h"
+
+VectorStorer::VectorStorer(std::size_t n) {
+    num_vecs = n;
+    for (int i = 0; i < num_vecs; ++i) {
+        x.push_back({0.0, 0.0, 0.0});
+    }
+}
+
+VectorStorer::VectorStorer(CLikeArray &c_like_array) {
+    num_vecs = c_like_array.num_vecs;
+    x = std::vector<std::array<double, 3>>(reinterpret_cast<std::array<double, 3> *>(c_like_array.x),
+                                           reinterpret_cast<std::array<double, 3> *>(c_like_array.x) + num_vecs);
+}
+
+void VectorStorer::copy_into_array(CLikeArray &c_like_array) {
+    for (std::size_t i = 0; i < num_vecs; ++i) {
+        for (std::size_t j = 0; j < 3; ++j) {
+            c_like_array.x[3 * i + j] = x[i][j];
+        }
+    }
+}
+
+VectorStorer VectorStorer::from_array(CLikeArray &c_like_array) {
+    auto vec_store = VectorStorer(c_like_array.num_vecs);
+    for (std::size_t i = 0; i < c_like_array.num_vecs; ++i) {
+        for (std::size_t j = 0; j < 3; ++j) {
+            vec_store.x[i][j] = c_like_array.x[3 * i + j];
+        }
+    }
+    return vec_store;
+}
+
+void explicit_copy(CLikeArray &in, CLikeArray &out) {
+    auto vec_store = VectorStorer::from_array(in);
+    vec_store.copy_into_array(out);
+}
+
+void implicit_copy(CLikeArray &in, CLikeArray &out) {
+    auto vec_store = VectorStorer(in);
+    vec_store.copy_into_array(out);
+}
---
+++ array_recasting.h
@@ -0,0 +1,31 @@
+#ifndef CXX_LIBRARY_TESTS_ARRAY_RECASTING_H
+#define CXX_LIBRARY_TESTS_ARRAY_RECASTING_H
+
+#include <vector>
+#include <array>
+
+extern "C" {
+struct CLikeArray {
+    std::size_t num_vecs;
+    double *x;
+};
+void explicit_copy(CLikeArray &in, CLikeArray &out);
+void implicit_copy(CLikeArray &in, CLikeArray &out);
+}
+
+class VectorStorer {
+public:
+    std::size_t num_vecs;
+    std::vector<std::array<double, 3>> x;
+
+
+    explicit VectorStorer(std::size_t n);
+
+    explicit VectorStorer(CLikeArray &c_like_array);
+
+    void copy_into_array(CLikeArray &c_like_array);
+
+    static VectorStorer from_array(CLikeArray &c_like_array);
+};
+
+#endif //CXX_LIBRARY_TESTS_ARRAY_RECASTING_H
