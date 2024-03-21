77fe1bb559ee6774bea8e5243c9b6341e742a66d
Joshua DM Hellier <joshua.hellier-2@manchester.ac.uk>
2024-03-21 21:11:46 +0000

    Add stuff to add seven to every element in an array.

---
+++ array_seven.cpp
@@ -0,0 +1,7 @@
+#include "array_seven.h"
+
+void add_seven(std::size_t array_size, double *in, double *out) {
+    for (size_t i = 0; i < array_size; ++i) {
+        out[i] = in[i] + 7.0;
+    }
+}
---
+++ array_seven.h
@@ -0,0 +1,10 @@
+#ifndef CXX_LIBRARY_TESTS_ARRAY_SEVEN_H
+#define CXX_LIBRARY_TESTS_ARRAY_SEVEN_H
+
+#include <cstddef>
+
+extern "C" {
+void add_seven(std::size_t array_size, double *in, double *out);
+}
+
+#endif //CXX_LIBRARY_TESTS_ARRAY_SEVEN_H
