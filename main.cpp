1d4e78037883a827a955ed1bd75afa58a676fd8c
Joshua DM Hellier <joshua.hellier-2@manchester.ac.uk>
2024-03-27 11:13:27 +0000

    Remove unwanted files.

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "add_seven.h"
-
-int main() {
-    int my_int = 3;
-    int your_int = add_seven(my_int);
-    std::cout << "Hello, World!" << your_int << std::endl;
-    return 0;
-}
